c5db0e9b6c9cdc753449dd9af6243aea0f9f8ee5
vsunday <v.sunday125@gmail.com>
2020-08-31 10:15:58 +0000

    change port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require("express"),
       {v4: uuidv4} = require("uuid");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(require("express-session")({
